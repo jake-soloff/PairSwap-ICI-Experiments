7b6c45fab21e9fd78ad560eda434175daf98b074
Rohan Hore <39278210+rohanhore@users.noreply.github.com>
2025-04-30 09:43:29 -0500

    adding comp

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import networkx as nx
 import random
 import statistics as stat
+from scipy.stats import norm
 
 
 def cross_bin_matching(Y, Z, K):
@@ -65,7 +66,7 @@ def neighbour_matching(Y, Z):
     return M
 
 
-def PairSwapICI_test(X, Y, M, nperm=1000):
+def PairSwapICI_test(X, Y, M, Del_W, Y_weights=False, nperm=1000):
     """
     Perform the SCINT test on paired differences.
 
@@ -73,6 +74,7 @@ def PairSwapICI_test(X, Y, M, nperm=1000):
     X (array-like)
     Y (array-like)
     M (list of tuples): Matched pairs of indices.
+    W (list of 
     nperm (int): Number of random swaps for the test.
 
     Returns:
@@ -80,13 +82,15 @@ def PairSwapICI_test(X, Y, M, nperm=1000):
     """
     # Compute paired differences
     Del_X = np.array([X[i] - X[j] for (i, j) in M])
-    Del_Y = np.array([Y[i] - Y[j] for (i, j) in M])
+    if Y_weights == True:
+        Del_W = np.array([Y[i] - Y[j] for (i, j) in M])
+    
 
     # Compute randomly swapped test statistics
     D_S = np.einsum('ij, j, j -> i',
                     (np.random.rand(nperm, len(M)) < 0.5),
                     Del_X,
-                    Del_Y)
+                    Del_W)
 
     # Compute p-value
     return (1 + np.sum(D_S <= 0)) / (1 + nperm)
@@ -111,3 +115,54 @@ def marg_indep_test(X, Y, nperm=1000):
 
     # Compute p-value
     return (1 + np.sum(T_S >= T)) / (1 + nperm)
+
+def GCM_test(X, Y, Z, mu_X, mu_Y):
+    """
+    Perform GCM test.
+
+    Parameters:
+    X (array-like)
+    Y (array-like)
+    Z (arry-like)
+    mu_X (function): estimate of E(X|Z)
+    mu_Y (function): estimate of E(Y|Z)
+
+    Returns:
+    float: p-value for the test.
+    """
+    
+    # Compute residuals
+    res_X = X - mu_X(Z)
+    res_Y = Y - mu_Y(Z)
+    del_xy = res_X * res_Y
+    n = len(del_xy)
+    
+    mean_del = np.mean(del_xy)
+    std_del = np.std(del_xy, ddof=1)
+
+    # test_statistic 
+    test_stat = (np.sqrt(n) * mean_del) / std_del if std_del != 0 else np.inf
+    # p-value
+    return 1 - norm.cdf(test_stat)
+    
+    
+# NB: E should encode Z[i] preceq Z[j]
+def max_weight_matching(E, V):
+    n = E.shape[0]
+    W = np.maximum(E, 0)**2 / V
+    W = np.maximum(W, W.T)
+    
+    G = nx.Graph()
+    for i in range(1,n):
+        for j in range(i):
+            G.add_edge(i, j, weight=W[i,j])
+
+    M0 = nx.max_weight_matching(G)
+    M = []
+    for (i, j) in M0:
+        if E[i,j] > 0: 
+            M.append((i,j))
+        elif E[i,j] < 0:
+            M.append((j,i))
+
+    return(M)
